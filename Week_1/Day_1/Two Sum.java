40a120a56dc63ce19048542dc9bb9a2ea419b51d
ritikbhandari911 <ritikbhandari911@gmail.com>
2026-06-06 08:51:45 +0530

    Added Two Sum solution

--- Week_1/Day_1/Two Sum.java
+++ Week_1/Day_1/Two Sum.java
@@ -1,1 +1,21 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
 
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+
+            if(map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
