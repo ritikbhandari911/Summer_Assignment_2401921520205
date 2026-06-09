388fa187d0e58645b75e1899ece8c0b9ccc18321
ritikbhandari911 <ritikbhandari911@gmail.com>
2026-06-09 12:06:41 +0530

    Implement canConstruct method for RansomNote
    Solution of Ransom note problem

--- Week_2/Day_2/RansomNote.java
+++ Week_2/Day_2/RansomNote.java
@@ -1,1 +1,24 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        //step 1 -count the letters of both the strings 
+        for ( int i = 0 ; i < magazine.length() ; i++){
+            count[magazine.charAt(i) - 'a']++;
+        }
+        // step 2 - use the letters of magazine for ransomNote
+        for ( int i = 0 ; i < ransomNote.length() ; i++){
+            char c = ransomNote.charAt(i);
+            if(count[c - 'a'] == 0 ){
+                return false;
+
+            }
+            count[c - 'a']--;
+
+         }
+         return true;
+        }
+        
+    }
 
