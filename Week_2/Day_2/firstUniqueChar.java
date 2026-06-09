41f41ae6a099d3465a30d6c1d2b7a7065c90b1a2
ritikbhandari911 <ritikbhandari911@gmail.com>
2026-06-09 11:53:22 +0530

    Implement first unique character finder
    solution for first unique character problem

--- Week_2/Day_2/firstUniqueChar.java
+++ Week_2/Day_2/firstUniqueChar.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
 
+        int[] count = new int[26];
+
+
+        //step 1 - count frequency 
+        for( int i = 0 ; i < s.length() ; i++){
+            count[s.charAt(i) - 'a']++;
+        }
+        //step 2 - find the first non repeating letter
+        for ( int i = 0 ; i<s.length() ; i++){
+            if(count[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
